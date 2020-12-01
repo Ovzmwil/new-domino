2a4682c7bbb9f84582547698437aad7a846f0ad1
Leandro Santana <leandro.santana@clear.sale>
2020-12-01 19:24:49 -0300

    Novos usuários mockados

--- js/Server/Database/DB.js
+++ js/Server/Database/DB.js
@@ -3,10 +3,15 @@ const User = require('../Models/User');
 const DB = {
     "games" : [],
     "users" : [
-        new User(1, "vitor", "Vitor"),
-        new User(2, "akamine", "Akamine"),
-        new User(3, "irineu", "Irineu"),
-        new User(4, "arima", "Arima")
+        new User(1, "jones", "Jones"),
+        new User(2, "allan", "Allan"),
+        new User(3, "joão", "João"),
+        new User(4, "matheus", "Matheus"),
+        new User(5, "vinicius", "Vinicius"),
+        new User(6, "patrick", "Patrick"),
+        new User(7, "lucas", "Lucas"),
+        new User(8, "dominando", "Dominando"),
+        new User(9, "leandro", "Leandro")
     ]
 };
 module.exports = DB;
